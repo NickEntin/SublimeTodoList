b542a8aaa0ea5b0161b6fbb38647ca7cb45be300
Nick Entin <Nick Entin@nickentin-pc.(none)>
2014-07-31 22:33:08 -0700

    scan file on load and modified

--- TodoList.py
+++ TodoList.py
@@ -1,6 +1,12 @@
 import sublime, sublime_plugin
 
 class TodoList(sublime_plugin.EventListener):
-	def on_modified(self, view):
+	def scan_file(self, view):
 		items = view.find_all("TODO")
 		view.add_regions("todo", items, "todo", "bookmark", sublime.HIDDEN)
+
+	def on_modified(self,view):
+		self.scan_file(view);
+
+	def on_load(self,view):
+		self.scan_file(view);
